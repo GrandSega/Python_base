e4289c318556cf4c1dfe9fc7c444b99960d8b99d
GrandSega <grandsega@mail.com>
2022-01-22 19:50:33 +0300

    Yjdfz dthcbz

--- Leonovich_Sergey_DZ_1/task_1.py
+++ Leonovich_Sergey_DZ_1/task_1.py
@@ -1,24 +1,25 @@
 print("Укажите количество секунд которое хотите конвертироовать :")
-time_to_convert = int(input())
-def convert_time(sec):
-    if sec >= 60:
-        min = int(sec / 60)
-        secunds = sec - (min * 60)
+duration = int(input())
+def convert_time(duration: int) -> str:
+    if duration >= 60:
+        min = int(duration / 60)
+        secunds = duration - (min * 60)
         print(str(secunds) + " сек")
         print(str(min) + " мин ")
     else:
-        print(str(sec) + " сек")
+        print(str(duration) + " сек")
     hours = int(min / 60)
     if hours > 0:
         print(str(hours) + " час")
         day = int(hours / 24)
         if day > 0:
-            print(str(day) + " дня")
+            print(str(day) + " дн")
     else:
         print("Часы не найдены")
-    week = int( day / 7)
+    week = int(day / 7)
     if week > 0:
         print(str(week) + " нед")
     else:
         print("Недель не найдено")
-convert_time(time_to_convert)
+result = convert_time(duration)
+print(result)
